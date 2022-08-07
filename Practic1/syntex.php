b1330bdbea8a89d128aec1dff91e65605887c59f
subroto suvro <47747335+suvro1999@users.noreply.github.com>
2022-08-07 21:26:25 +0600

    Declearation the multiple types of variable

--- Practic1/syntex.php
+++ Practic1/syntex.php
@@ -10,6 +10,29 @@
     <?php
     //This is practice of how to print "hello world" in PHP or check out about php syntax 
     echo "Hello World";
+    /*this is example of multiple line comment
+    in php */
+
+    //Now practicting the variable
+    //How to take variable? Variable always start with $ sign and variable name always start without number,symbol. 
+    //Variable are case sencetive 
+
+    $int_number = 1999;
+    $float = 19.99;
+    $string = "Subrato";
+    $null = NULL;
+    
+
+    echo $int_number;
+    echo "<br>";
+    echo $float;
+    echo "<br>";
+    echo $string;
+    echo "<br>";
+    echo $null;
+    
+    //End of variable declearation
+    
     ?>
 </body>
 </html>

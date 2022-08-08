ea848229f553809a8b93a70e8b5289407acf1f4a
subroto suvro <47747335+suvro1999@users.noreply.github.com>
2022-08-08 17:50:07 +0600

    Adding operators.php file and Practicing Operators in details

---
+++ Practic1/operators.php
@@ -0,0 +1,62 @@
+<?php
+
+$x = 4;
+$y = 5;
+echo ($x-$y);
+echo "<br>";
+// Output will be -1 by sub
+
+echo ($x+$y);
+echo "<br>";
+
+// Out put will be 9 by adding those two variables
+echo ($x/$y);
+echo "<br>";
+
+//Output will be 1.25 or something by dividing 
+echo ($x%$y);
+echo "<br>";
+
+echo ($x*$y);
+echo "<br>";
+// Out put will be 20 by multiple those two varialbe
+echo ($x**$y);
+echo "<br>";
+
+
+
+//Now let's practices the other logical operation like and or 
+
+echo ($x&&$y);
+echo "<br>";
+//this is and operators 
+
+echo ($x||$y);
+echo "<br>";
+//THis is or operators
+
+echo ($x==$y);
+echo "<br>";
+//Equal Opretor
+
+echo ($x!=$y);
+echo "<br>";
+//This is not equal operetor 
+
+echo ($x===$y);
+echo "<br>";
+//this is identical operator 
+
+echo ($x>$y);
+echo ($x<$y);
+echo "<br>"; 
+//this is gater than and lessthan operator/
+
+echo ($x<=$y);
+echo ($x>=$y);
+echo "<br>";
+//This is gaterthen equal and lessthan equal Operators.
+
+
+
+?>
